a725cc98ea90b3911c2467232503f642238fda88
Tanvir Ahmmed <ahmed.tanvir.010@gmail.com>
2018-03-08 01:50:38 +0600

    [initiate]

---
+++ resources/assets/js/components/ExampleComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+                <div class="alert alert-primary" role="alert">
+                    This is a primary alert—check it out!
+                </div>
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
